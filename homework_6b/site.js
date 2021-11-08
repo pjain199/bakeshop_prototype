b779156e556246c2cfc1e5cb8174efcb544ecb9d
Priya Jain <priyajain@Priyas-MacBook-Pro.local>
2021-11-07 19:51:41 -0500

    final touches

--- homework_6b/site.js
+++ homework_6b/site.js
@@ -1,149 +1,5 @@
-
-// function passValue(){
-//   var selectInfo = document.getElementById("glaze").value;
-//   localStorage.setItem("glazeValue", selectInfo);
-
-//   var selectAmountInfo = document.getElementById("buns").value;
-//   localStorage.setItem("amountValue", selectAmountInfo);
-
-//   return true;
-
-// }
-
-// // var shoppingCart = (function() {
-// //   cart = [];
-
-//   // function Item(name, amount, count) {
-//   //   this.name = function(){
-//   //     return document.getElementsByClassName("flavor");
-//   //   };
-//   //   this.amount = function(){
-//   //     passValue();
-//   //   };
-//   //   this.count = count;
-//   // }
-
-
-  
-
-
-
-
-// //contructor
-//   function Item(name, amount, count) {
-//     this.name = name;
-//     this.amount = amount;
-//     this.count = count;
-//   }
-
-//   var item = new Item (document.getElementsByClassName("flavor"), passValue(), count);
-
-//   var cart = []; 
-
-  
-
-//   cart.push(item);
-
-//   function saveCart() {
-//     localStorage.setItem('shoppingCart', JSON.stringify(cart));
-//   }
-  
-//     // Load cart
-//   function loadCart() {
-//     cart = JSON.parse(sessionStorage.getItem('shoppingCart'));
-//   }
-
-
-
-
-
-// }
-
-// document.getElementById("result").innerHTML = localStorage.getItem("glazeValue");
-
-
-
-// var slideIndex = 1;
-// showSlides(slideIndex);
-
-// function plusSlides(n) {
-//   showSlides(slideIndex += n);
-// }
-
-// function currentSlide(n) {
-//   showSlides(slideIndex = n);
-// }
-
-// function showSlides(n) {
-//   var i;
-//   var slides = document.getElementsByClassName("mySlides");
-//   var dots = document.getElementsByClassName("dot");
-//   if (n > slides.length) {slideIndex = 1}
-//     if (n < 1) {slideIndex = slides.length}
-//     for (i = 0; i < slides.length; i++) {
-//       slides[i].style.display = "none";
-//     }
-//     for (i = 0; i < dots.length; i++) {
-//       dots[i].className = dots[i].className.replace(" active", "");
-//     }
-//   // slides[slideIndex-1].style.display = "block";
-//   dots[slideIndex-1].className += " active";
-// }
-
-// var modal = document.getElementById('productDetailsModal');
-// var originalPic = document.getElementById('originalFlavor');
 var closeBtn = document.getElementsByClassName('closeBtn')[0];
 
-// originalPic.addEventListener('click', openModal);
-
-// const addToCartButton = document.getElementById("add-to-cart");
-
-// function Item(name, glaze, amount) {
-//   this.name = name;
-//   this.glaze = glaze;
-//   this.amount = amount;
-// }
-
-// while(addToCartButton){
-//   var clicked = false;
-//   addToCartButton.addEventListener("click", function() {
-//     clicked = true;
-//   }​
-//  if (clicked===true){
-//   var item1 = new Item("h1", (document.querySelector('#glaze')).value, (document.querySelector('#buns')).value);
-//   sessionStorage.setItem('shoppingCart', JSON.stringify(item1));
-//   console.log(JSON.parse(sessionStorage.getItem('shoppingCart')));
-//  }
-
-// }
-
-
-// function get() {
-//   console.log(JSON.parse(sessionStorage.getItem('shoppingCart')));
-// }
-
-
-
-
-
-// var item1 = new Item("h1", document.querySelector('#glaze'), document.querySelector('#buns'));
-// sessionStorage.setItem('shoppingCart', JSON.stringify(item1));
-// console.log(JSON.parse(sessionStorage.getItem('shoppingCart')));
-
-
-// closeBtn.addEventListener('click', closeModal);
-
-// var cartInfo = [];
-
-// const cartItem = {
-//   name = document.getElementById('productName').innerHTML,
-//   glaze = document.getElementById("glaze").value,
-//   amount = document.getElementById("buns").value
-// };
-
-// const item = Object.create(cartItem);
-// cartInfo.push(item);
-// console.log(cartInfo);
 
 function openModal(productProperties){
   var modal = document.getElementById('productDetailsModal');
@@ -158,16 +14,6 @@ function closeModal(){
 
 }
 
-//confused with saving cart number to next page
-// let count = 0;
-
-
-// <div class = item>
-// <img class = "cartImg" src="source/bun.svg" alt="product picture in cart">
-// <h3 class = 'itemHeading'>Original</h3>
-// <p class = 'details'>Topping: Vanilla Glaze <br> </br>Amount: 3 buns <br> </br> Price: $15.00 </p>
-// <p class = 'remove'>Remove</p>
-// </div>
 
 function Load(){
 let cartInfo = JSON.parse(localStorage.getItem("cartInfo"));
@@ -177,12 +23,10 @@ for (var i = 0; i < cartInfo.length; i++) {
   var cartInfoItem = cartInfo[i]
   var cartInfoItemDiv = document.createElement("div")
   document.getElementById('counter-display').innerHTML = cartInfo.length;
-  // var cartNumber = document.getElementById('counter-display').innerHTML
   cartInfoItemDiv.classList.add("item")
   var cartInfoItemImage = document.createElement("img")
   cartInfoItemImage.src = "source/bun.svg"
   cartInfoItemImage.classList.add("cartImg")
-// figure out how to add alt text attribute
   var cartInfoItemTitle = document.createElement("h3")
   cartInfoItemTitle.classList.add("itemHeading")
   cartInfoItemTitle.innerText = cartInfoItem["flavor"]
@@ -208,25 +52,15 @@ for (var i = 0; i < cartInfo.length; i++) {
       removeItem(cartInfoItem)
 
       cartInfoItemDiv.parentNode.removeChild(cartInfoItemDiv)
-      // cartNumber = cartNumber - 1;
-      // document.getElementById('counter-display').innerHTML = cartInfo.length;
 
-      console.log(cartInfo);
-      console.log(cartInfo.length)
+      // console.log(cartInfo);
+      // console.log(cartInfo.length)
 
     }
-    // document.getElementById('counter-display').innerHTML = cartInfo.length;
 
    }(cartInfoItem, cartInfoItemDiv))
 
 
-
-
-
-
-  //click on remove text to remove object in array
- 
-  // attachmentNode.appendChild()
 }
 
 
@@ -241,14 +75,13 @@ function increaseCounter(counter){
   document.getElementById('counter-display').innerHTML = counter;
 }
 
-//removes item
-function removeItem(obj) {
-  // retrieve the stored value of the cart items so that we can modify it
+//removes item - adapted from Jesse's codepen
+function removeItem (obj) {
+ 
   var cartItemsString = localStorage.getItem("cartInfo")
-  if (cartItemsString !== null) {
-    var cartInfo = JSON.parse(cartItemsString) // successfully loaded in the cart items
+  if(cartItemsString !== null) {
+    var cartInfo = JSON.parse(cartItemsString) 
     
-    // find the index of the input object in the list
     var ind = cartInfo.findIndex(function (item) {
       return obj.flavor === item.flavor && item.glaze === obj.glaze && item.amount === obj.amount
     })
@@ -259,7 +92,6 @@ function removeItem(obj) {
       // update the stored value
       localStorage.setItem("cartInfo", JSON.stringify(cartInfo))
       // re-render the page to reflect changes
-      // Load()
     }
   }
   document.getElementById('counter-display').innerHTML = cartInfo.length;
@@ -292,21 +124,6 @@ function addToCart(){
   document.getElementById('counter-display').innerHTML = cartInfo.length;
 
 }
-  //is this right? for loop maybe?
-  // for every item in Array
-  // print onto cart page
-  // onclick remove function (remove by id or index?)
-
-  // document.getElementById('cart-array').innerHTML = cartInfo;
-
-//   for (var i = 0; i < cartInfo.length; i++) {
-//     var added = cartInfo[i];
-//     var itemNode = document.createElement("li");
-//     itemNode.innerText = added.flavor + added.glaze + " (" + added.amount + ")";
-// }
-
-
-// localStorage.setItem("myItem", JSON.stringify(cartItem));
 
 JSON.parse(localStorage.getItem("cartInfo")); //where does this go?
 
